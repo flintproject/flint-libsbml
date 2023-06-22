77d5b0d8ad4853e2ba0ee1534655a922eb80b75f
Takeshi Abe <tabe@fixedpoint.jp>
2023-06-23 00:25:05 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A flake for building libsbml for Flint";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-23.05;
+
+  inputs.libsbml = {
+    url = github:sbmlteam/libsbml/186f62fc34f5836cff0ed162aa5e8b880f22fcb1;
+    flake = false;
+  };
+
+  outputs = { self, nixpkgs, libsbml }: let
+
+    allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+
+    forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f (import nixpkgs { inherit system; }));
+
+  in {
+
+    packages = forAllSystems (pkgs: with pkgs; let
+
+      flint-libsbml = stdenv.mkDerivation {
+        pname = "flint-libsbml";
+        version = "3.4.1";
+        nativeBuildInputs = [ pkg-config ];
+        buildInputs = [ libxml2 ];
+        checkInputs = [ check ];
+        src = libsbml;
+        # configureFlags = [ "--with-check" ];
+        # doCheck = true;
+      };
+
+    in {
+
+      default = flint-libsbml;
+
+    });
+  };
+}
